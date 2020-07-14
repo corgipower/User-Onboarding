1d802823c0b109f456cf163a331b17d285a7b4b7
Liz Shulman <corgipower@hotmail.com>
2020-07-14 01:52:28 -0400

    form built

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,3 +1,51 @@
+import React, {useState} from 'react';
+
 const Form = () => {
-    return <p>form</p>
+    const defaultState = {
+        name: '',
+        email: '',
+        password: '',
+        terms: false,
+    }
+
+    const [user, setUser] = useState(defaultState);
+
+    const handleChange = event => {
+        const value =
+        event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+        setUser({
+            ...user,
+            [event.target.name]: value
+        });
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log('submitted');
+        setUser(defaultState);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='name'>
+                Name:
+                <input type='text' name='name' value={user.name} onChange={handleChange} errors='' />
+            </label>
+            <label htmlFor='name'>
+                Email:
+                <input type='text' name='email' value={user.email} onChange={handleChange} errors='' />
+            </label>
+            <label htmlFor='name'>
+                Password:
+                <input type='text' name='password' value={user.password} onChange={handleChange} errors='' />
+            </label>
+            <label htmlFor='terms'>
+                Terms of Service
+                <input name='terms' type='checkbox' onChange={handleChange} />
+            </label>
+            <button disabled=''>Submit</button>
+        </form>
+    )
 }
+
+export default Form;
